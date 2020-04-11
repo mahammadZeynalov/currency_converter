d5ad0b1589eb06a3e0c4726d0b2819ec0d44f441
Mahammad Zeynalov <magamed9r2.2011@gmail.com>
2020-04-11 18:46:43 +0400

    webpack

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const charts = require('chart.js');
+
 const topCurrenciesFrom = document.querySelectorAll('.top-currencies-from .top-cur');
 const topCurrenciesTo = document.querySelectorAll('.top-currencies-to .top-cur');
 
@@ -190,25 +192,44 @@ function switcherDivHandler() {
     parent.append(right, center, left);
 }
 
-// async function loadCurrenciesRate() {
+// const chart = document.querySelector('.my-chart');
 
-// }
-
-// async function displayCurrentRate() {
-
-// }
-
-// function changeCurrencyPositions() {
-
-// }
-
-// function loadDelay() {
-
-// }
-
-// function isCurrenciesAreSame() {
-
-// }
+// var myChart = new Chart(chart, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
 
 
 
