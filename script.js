0cd9bf01a4cddb414a1c392150e611c43d933c3b
Mahammad Zeynalov <magamed9r2.2011@gmail.com>
2020-04-12 21:24:17 +0400

    Data visualization added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Declaring global variables.
-
+const Chart = require('chart.js');
 // RUB, USD, EUR, GBP
 const topCurrenciesFrom = document.querySelectorAll('.top-currencies-from .top-cur');
 const topCurrenciesTo = document.querySelectorAll('.top-currencies-to .top-cur');
@@ -29,6 +29,7 @@ let loadingTimer;
 // Convert the default input values (RUB/USD)
 window.addEventListener('load', (e) => {
     convertMoney(e);
+    drawTable(weeklyRequest);
 });
 
 // User can interact with app by 3 ways: 
@@ -83,6 +84,7 @@ function loadingHandler() {
 
 function convertHandler(e) {
     convertTimer = setTimeout(() => {
+        drawTable(weeklyRequest);
         convertMoney(e);
         loading.style.display = 'none';
         enableInputs();
@@ -248,55 +250,101 @@ function switcherDivHandler() {
     parent.append(right, center, left);
 }
 
-const time = new Date();
-// function prepareDates() {
-//     const time = new Date();
-//     let str1 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-//     time.setDate(time.getDate() - 1);
-//     let str2 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-//     time.setDate(time.getDate() - 1);
-//     let str3 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-//     time.setDate(time.getDate() - 1);
-//     let str4 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-//     time.setDate(time.getDate() - 1);
-//     let str5 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-//     time.setDate(time.getDate() - 1);
-//     let str6 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-//     time.setDate(time.getDate() - 1);
-//     let str7 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+// https://fixer.io/documentation flactuation
 
-//     let currencyFrom = detectSelectedCurrencies()[0];
-//     let currencyTo = detectSelectedCurrencies()[1];
-// }
+function prepareDates() {
+    let arr = [];
+    const time = new Date();
+    let str1 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    arr.push(str1);
+    time.setDate(time.getDate() - 1);
+    let str2 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    arr.push(str2);
+    time.setDate(time.getDate() - 1);
+    let str3 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    arr.push(str3);
+    time.setDate(time.getDate() - 1);
+    let str4 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    arr.push(str4);
+    time.setDate(time.getDate() - 1);
+    let str5 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    arr.push(str5);
+    time.setDate(time.getDate() - 1);
+    let str6 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    arr.push(str6);
+    time.setDate(time.getDate() - 1);
+    let str7 = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    arr.push(str7);
+    return arr;
+}
 
-const Chart = require('chart.js');
+async function weeklyRequest(prepareDates) {
+    let arr = prepareDates();
+    let currencyFrom = detectSelectedCurrencies()[0];
+    let currencyTo = detectSelectedCurrencies()[1];
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+    let dataEachDay = [];
 
-    // The data for our dataset
-    data: {
-        labels: ['hello', 'Fy', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Rate',
-            backgroundColor: 'rgb(208,236,222)',
-            borderColor: 'green',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 10,
-                    suggestedMax: 100
-                }
-            }]
-        }
+    for (let i = 0; i < arr.length; i++) {
+        let data = await (await fetch(`https://api.ratesapi.io/api/${arr[i]}?base=${currencyFrom}&symbols=${currencyTo}`)).json();
+        dataEachDay.push(data);
     }
-});
+    console.log(dataEachDay);
 
+    let rates = dataEachDay.map((day) => day.rates[currencyTo])
+    console.log(rates);
+
+    const min = Math.min.apply(null, rates);
+    const max = Math.max.apply(null, rates);
+    console.log(min, max);
+    return [min, max, dataEachDay];
+}
+
+async function drawTable(weeklyRequest) {
+    let currencyFrom = detectSelectedCurrencies()[0];
+    let currencyTo = detectSelectedCurrencies()[1];
+
+    let data = await weeklyRequest(prepareDates);
+    console.log(data);
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: [`${data[2][0].date}`,
+                    `${data[2][1].date}`,
+                    `${data[2][2].date}`,
+                    `${data[2][3].date}`,
+                    `${data[2][4].date}`,
+                    `${data[2][5].date}`,
+                    `${data[2][6].date}`],
+            datasets: [{
+                label: 'Rate',
+                backgroundColor: 'rgb(208,236,222)',
+                borderColor: 'green',
+                data: [`${data[2][0].rates[currencyTo]}`,
+                        `${data[2][1].rates[currencyTo]}`,
+                        `${data[2][2].rates[currencyTo]}`,
+                        `${data[2][3].rates[currencyTo]}`,
+                        `${data[2][4].rates[currencyTo]}`,
+                        `${data[2][5].rates[currencyTo]}`,
+                        `${data[2][6].rates[currencyTo]}`]
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        max: data[1],
+                        min: data[0],
+                        stepSize: (data[1] - data[0])/6
+                    }
+                }]
+            }
+        }
+    });
+}
